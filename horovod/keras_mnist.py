8e18ad8ef3767d125cee816b6b2605a140cd0efd
yangyuliufeng <qlw705706@gmail.com>
2021-05-21 19:12:34 +0800

    add horovod

--- horovod/keras_mnist.py
+++ horovod/keras_mnist.py
@@ -3,26 +3,23 @@ from keras.datasets import mnist
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
-from keras import backend as K
 import math
 import horovod.keras as hvd
 
 
-# initialize horovod
 hvd.init()
 
 batch_size = 128
 num_classes = 10
-# adjust number of epochs based on number of training processes
+
+# 根据process数量调整epochs
 epochs = int(math.ceil(12.0 / hvd.size()))
 
 # Input image dimensions
 img_rows, img_cols = 28, 28
 
-
 # The data, shuffled and split between train and test sets
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
-
 
 if K.image_data_format() == 'channels_first':
     x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
@@ -33,7 +30,6 @@ else:
     x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
     input_shape = (img_rows, img_cols, 1)
 
-
 x_train = x_train.astype('float32')
 x_test = x_test.astype('float32')
 x_train /= 255
@@ -42,11 +38,9 @@ print('x_train shape:', x_train.shape)
 print(x_train.shape[0], 'train samples')
 print(x_test.shape[0], 'test samples')
 
-
 # Convert class vectors to binary class matrices
 y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
-
 
 model = Sequential()
 model.add(Conv2D(32, kernel_size=(3, 3),
@@ -60,32 +54,24 @@ model.add(Dense(128, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(num_classes, activation='softmax'))
 
-
-# adjust learning rate based on number of training processes
+# 根据process数量调整学习率
 opt = keras.optimizers.Adadelta(1.0 * hvd.size())
 
-
-# add horovod Distributed Optimizer,
+# 定义分布式优化器
 opt = hvd.DistributedOptimizer(opt)
-
 
 model.compile(loss=keras.losses.categorical_crossentropy,
               optimizer=opt,
               metrics=['accuracy'])
 
-
 callbacks = [
-    # Horovod: broadcast initial variable states from rank 0 to all other processes.
-    # This is necessary to ensure consistent initialization of all workers when
-    # training is started with random weights or restored from a checkpoint.
+    # 将初始变量状态从worker 0广播到所有worker
     hvd.callbacks.BroadcastGlobalVariablesCallback(0),
 ]
 
-
-# Horovod: save checkpoints only on worker 0 to prevent other workers from corrupting them.
+# 只在worker 0上保存checkout point
 if hvd.rank() == 0:
     callbacks.append(keras.callbacks.ModelCheckpoint('./checkpoint-{epoch}.h5'))
-
 
 model.fit(x_train, y_train,
           batch_size=batch_size,
